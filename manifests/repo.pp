35f757e4051c95ae521e83a59d0f4263a13b3348
marpada <david.pando+github@gmail.com>
2015-04-07 16:07:37 +1200

    Add class parameter for distribution

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -9,11 +9,16 @@
 # [*component*]
 #   Specify which component to put the package in. This option will only works
 #   for aptly version >= 0.5.0.
+# [*distribution*]
+#   Specify a distribution name, e.g. squeeze,
+#   for flat repositories use ./ instead of distribution name
 #
 define aptly::repo(
   $component = '',
+  $distribution = '',
 ){
   validate_string($component)
+  validate_string($distribution)
 
   include ::aptly
 
@@ -24,10 +29,16 @@ define aptly::repo(
   } else{
     $component_arg = "-component=\"${component}\""
   }
+  if empty($distribution) {
+    $distribution_arg = ''
+  } else{
+    $distribution_arg = "-distribution=\"${distribution}\""
+  }
 
 
   exec{ "aptly_repo_create-${title}":
-    command => "${aptly_cmd} create ${component_arg} ${title}",
+    command =>
+      "${aptly_cmd} create ${distribution_arg} ${component_arg} ${title}",
     unless  => "${aptly_cmd} show ${title} >/dev/null",
     user    => $::aptly::user,
     require => [
